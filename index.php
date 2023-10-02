98dc33e74f0506e4057ea076984b0944c0444080
StanislavDitsan <ditsanstas@gmail.com>
2023-10-02 06:55:41 +0200

    test

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/d6e8932834.js" crossorigin="anonymous"></script>
-    <title>Hello, world!</title>
+    <title>Hello, world</title>
 </head>
 
 <body>
